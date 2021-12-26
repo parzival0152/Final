efc27bf6ac61a729da06a25d510264f3c9157da1
Ilay Tzuberi <ilay.tzu@gmail.com>
2021-12-26 16:24:50 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Length, Email
+
+
+
+app = Flask(__name__)
+Bootstrap(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+app.config["SECRET_KEY"] = "hello"
+db = SQLAlchemy(app)
+
+class SigninForm(FlaskForm):
+    username = StringField("username", validators=[InputRequired(),Length(min = 4, max= 30)])
+    password = PasswordField("password", validators=[InputRequired(),Length(min=4,max=50)])
+    rememberme = BooleanField("remember me")
+
+class SignupForm(FlaskForm):
+    username = StringField("username", validators=[InputRequired(),Length(min = 4, max= 30)])
+    username = StringField("email", validators=[InputRequired(),Length(min = 4, max= 255),Email(message="invalid email")])
+    password = PasswordField("password", validators=[InputRequired(),Length(min=4,max=50)])
+
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    Uid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), nullable=False)
+    pwd = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
+    fullName = db.Column(db.String(255))
+
+
+class Template(db.Model):
+    __tablename__ = "templates"
+    Tid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    owner = db.Column(db.Integer, db.ForeignKey("users.Uid"))
+    data = db.Column(db.JSON, nullable=False)
+    stats = db.Column(db.JSON, nullable=False)
+
+
+class Document(db.Model):
+    __tablename__ = "documets"
+    Did = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.JSON, nullable=False)
+    owner = db.Column(db.Integer, db.ForeignKey("users.Uid"))
+    master = db.Column(db.Integer, db.ForeignKey("templates.Tid"))
+    stage = db.Column(db.Integer)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/signin")
+def signin():
+    form = SigninForm()
+
+    return render_template("signin.html",form = form)
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
